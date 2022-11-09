91fd0ed7dd40a9a6f55c74ac0fae268ca2d18d68
chinthu <abhishekbh0476@gmail.com>
2022-11-09 14:29:09 +0530

    change1

---
+++ chinthu.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+d ={'Name':pd.Series(['Tom','James','Ricky','Vin','Steve','Smith','Jack',
+'Lee','Chanchal','Gasper','Naviya','Andres']),
+'Age':pd.Series([25,26,25,23,30,29,23,34,40,30,51,46]),
+'Rating':pd.Series([4.23,3.24,3.98,2.56,3.20,4.6,3.8,3.78,2.98,4.80,4.10,3.65])}
+
+#Create a DataFrame
+df=pd.DataFrame(d)
+print("Mean Values in the Distribution")
+print(df.mean())
+print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+print("Mode Values in the Distribution")
+print(df.mode())
